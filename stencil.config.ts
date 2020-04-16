986c5e0722f63140643975b1edae2b08b9b55ca6
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-04-16 10:54:35 +0200

    build: stencil sass

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,10 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'windows-developer-stencil-components',
   taskQueue: 'async',
+  plugins: [sass()],
   outputTargets: [
     {
       type: 'dist',
